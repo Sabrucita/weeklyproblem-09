89f9186f34f2d3d5c230433541eae2ad882888d3
Sabrina <pereira.tsabrina@gmail.com>
2021-04-05 10:29:36 -0300

    test validation message changed

--- js/register.js
+++ js/register.js
@@ -1,18 +1,18 @@
 function formExists(){
 	formFound= document.querySelector("form");
 	if (formFound == null){
-		console.log("Form doesn't exist");
+		//console.log("Form doesn't exist");
 		results.innerHTML = "Form doesn't exist. ";
 	} else{
-		console.log("Form exist");
-		results.innerHTML = "<li>Form exist. </li>";
+		//console.log("Form exist");
+		//results.innerHTML = "<li>Form exist. </li>";
 	}
 }
 function countFields(expectedResult){
 	countInput= document.querySelectorAll("form input");
 	if (countInput.length == expectedResult){
-		console.log("Quantity of fields correct");
-		results.innerHTML = results.innerHTML+"<li> Quantity of fields correct. </li>";
+		//console.log("Quantity of fields correct");
+		//results.innerHTML = results.innerHTML+"<li> Quantity of fields correct. </li>";
 	} else{
 		console.log("Quantity of fields is not correct")
 		results.innerHTML = results.innerHTML+"<li> Quantity of fields is not correct. </li>";
@@ -27,16 +27,16 @@ function checkRequired(requiredId,required){
 		if(element.id == requiredId){
 			found = true;
 			if (element.required == required){
-				console.log(requiredId+": Found id and correct required value");
-				results.innerHTML = results.innerHTML+requiredId+": <li>Found id and correct required value. </li>";
+				//console.log(requiredId+": Found id and correct required value");
+				//results.innerHTML = results.innerHTML+requiredId+": <li>Found id and correct required value. </li>";
 			} else {
-				console.log(requiredId+": Found id and incorrect required value");
+				//console.log(requiredId+": Found id and incorrect required value");
 				results.innerHTML = results.innerHTML+requiredId+": <li>Found id and incorrect required value. </li>";
 			}
 		}
 	}
 	if(found == false){
-		console.log(requiredId+": Id not found");
+		//console.log(requiredId+": Id not found");
 		results.innerHTML = results.innerHTML+requiredId+": <li>Id not found. </li>";
 	}
 }
@@ -50,19 +50,26 @@ function checkLabels(){
 			const labelFor = foundLabels[index].getAttribute("for");
 			if(id == labelFor){
 				found = true;
-				console.log(id,labelFor+": Element id has an associated label");
-				results.innerHTML = results.innerHTML+id+": <li>Element id has an associated label. </li>";
+				//console.log(id,labelFor+": Element id has an associated label");
+				//results.innerHTML = results.innerHTML+id+": <li>Element id has an associated label. </li>";
 			}
 		}
 		if (found == false){
-			console.log(id+": Id does not have an associated label");
+			//console.log(id+": Id does not have an associated label");
 			results.innerHTML = results.innerHTML+id+": <li>Id does not have an associated label. </li>";
 		}
 	}
 }
 results = document.querySelector('.container ul');
-console.log(results);
+//console.log(results);
 
+
+function validationTests(){
+	container = document.querySelector('.container');
+	if(container.textContent == ""){
+		container.innerHTML = "validations results: every validation has passed.";
+	}
+}
 
 formExists();
 countFields(4);
@@ -71,3 +78,4 @@ checkRequired("email",true);
 checkRequired("password",true);
 checkRequired("repeatPassword",true);
 checkLabels();
+validationTests();
