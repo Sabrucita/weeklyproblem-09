654f21e70590087d38922ba8f5e47e05cee0481c
Sabrina <pereira.tsabrina@gmail.com>
2021-04-09 23:48:38 -0300

    login validations and blur/focus

--- js/login-validation.js
+++ js/login-validation.js
@@ -1,7 +1,41 @@
-function validarEmail(valor) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
-     alert("La dirección de email " + valor + " es correcta.");
-    } else {
-     alert("La dirección de email es incorrecta.");
-    }
+document.getElementById("email").addEventListener("blur", emailValidation);
+document.getElementById("password").addEventListener("blur", passwordValidation);
+
+document.getElementById("email").addEventListener("focus", emailFocus);
+document.getElementById("password").addEventListener("focus", passwordFocus);
+
+function emailFocus(e) {
+  var errorMessage = document.querySelector('#emailError');
+  errorMessage.classList.add("hidden");
+}
+
+function passwordFocus(e) {
+  var errorMessage = document.querySelector('#passwordError');
+  errorMessage.classList.add("hidden");
+}
+
+function emailValidation(e){
+  var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  var esValido = expReg.test(e.target.value);
+  var errorMessage = document.querySelector('#emailError');
+  if (esValido==false){
+    errorMessage.innerHTML = "The email adress is invalid";
+    errorMessage.classList.remove("hidden");
   }
+  else{
+    errorMessage.classList.add("hidden");
+  }
+}
+
+function passwordValidation(e){
+  var expReg = /^[a-zA-Z0-9]{8,}$/;
+  var esValido = expReg.test(e.target.value);
+  var errorMessage = document.querySelector('#passwordError');
+  if (esValido==false){
+    errorMessage.innerHTML = "The password is invalid";
+    errorMessage.classList.remove("hidden");
+  }
+  else{
+    errorMessage.classList.add("hidden");
+  }
+}
